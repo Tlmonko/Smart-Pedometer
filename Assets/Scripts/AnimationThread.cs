8425cf20bbbd2d9e83020a7ac7e5be4762d71590
Timofey <ktkozlov@yandex.ru>
2019-01-18 16:05:29 +0300

    V2

---
+++ Assets/Scripts/AnimationThread.cs
@@ -0,0 +1,22 @@
+using System.Threading;
+
+
+public static class AnimationThread
+{
+    private static readonly Thread AnimationThr;
+
+    static AnimationThread()
+    {
+        AnimationThr = new Thread(Runner);
+    }
+
+    private static void Runner(object obj)
+    {
+        
+    }
+
+    public static void Start()
+    {
+        AnimationThr.Start();
+    }
+}
